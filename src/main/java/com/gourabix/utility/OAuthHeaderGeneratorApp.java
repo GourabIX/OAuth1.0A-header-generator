c97d6feadc47e09fb9780117961eecebe614cc4d
Gourab Sarkar <gourabsarkar.x@gmail.com>
2022-02-28 22:31:10 +0530

    Updated main class.

--- src/main/java/com/gourabix/utility/OAuthHeaderGeneratorApp.java
+++ src/main/java/com/gourabix/utility/OAuthHeaderGeneratorApp.java
@@ -1,11 +1,24 @@
 package com.gourabix.utility;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+
+import com.gourabix.utility.oauth10a.OAuth10AHeaderGenerator;
+
 /**
- * Hello world!
+ * The entrypoint for the OAuthHeaderGeneratorApp.
+ * 
+ * @author gourab
  *
  */
 public class OAuthHeaderGeneratorApp {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException {
+		OAuth10AHeaderGenerator oAuth10AHeaderGenerator = new OAuth10AHeaderGenerator("test", "test");
+		String authHeader = oAuth10AHeaderGenerator.generateHeader("GET", "https://www.google.com/test",
+				new HashMap<>());
+		System.out.println(authHeader);
 	}
+
 }
